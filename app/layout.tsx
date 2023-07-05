90463a20a6d0476cfdcefa5704f3801b195dd4b2
sowmik-sec <sowmik.sec@gmail.com>
2023-07-05 11:04:59 +0600

    initializing the project

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+export const metadata = {
+  title: "Flexible",
+  description: "Showcase and discover remarkable developer projects",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        Navbar
+        <main>{children}</main>
+        Footer
+      </body>
+    </html>
+  );
+}
