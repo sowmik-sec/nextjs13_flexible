4a933203bbfc9f2758c48c248308586a052d4662
sowmik-sec <sowmik.sec@gmail.com>
2023-07-05 11:23:55 +0600

    add constants

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,16 @@
+import Link from "next/link";
+import Image from "next/image";
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <nav className="flexBetween navbar">
+      <div className="flex-1 flexStart gap-10">
+        <Link href="/">
+          <Image src="/logo.svg" width={115} height={43} alt="Flexible" />
+        </Link>
+        <ul className="xl:flex hidden text-small gap-7"></ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
